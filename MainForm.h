f5f8083e3b1e60828f51dc062cf210e9acc4325e
Anderson Assis <anderson.assis1989@yahoo.com>
2022-07-22 21:53:48 -0400

    First Commit

---
+++ MainForm.h
@@ -0,0 +1,289 @@
+#pragma once
+
+namespace CalculadoraPrimeiraLeideOhm {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+
+	/// <summary>
+	/// Sumário para MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Adicione o código do construtor aqui
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox4;
+
+	private:
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::Black;
+			this->label1->Location = System::Drawing::Point(72, 34);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(401, 29);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Calculadora Primeira Lei de Ohm";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::Black;
+			this->label2->Location = System::Drawing::Point(72, 137);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(79, 25);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Tensão";
+			this->label2->Click += gcnew System::EventHandler(this, &MainForm::label2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::Color::Black;
+			this->label3->Location = System::Drawing::Point(72, 210);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(88, 25);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Corrente";
+			this->label3->Click += gcnew System::EventHandler(this, &MainForm::label3_Click);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->ForeColor = System::Drawing::Color::Black;
+			this->label4->Location = System::Drawing::Point(54, 281);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(112, 25);
+			this->label4->TabIndex = 3;
+			this->label4->Text = L"Resistência";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::Color::Black;
+			this->button1->Location = System::Drawing::Point(195, 424);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(147, 49);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Calcular";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox1->Location = System::Drawing::Point(172, 141);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(275, 27);
+			this->textBox1->TabIndex = 5;
+			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox2->Location = System::Drawing::Point(172, 214);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(275, 27);
+			this->textBox2->TabIndex = 6;
+			this->textBox2->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox3->Location = System::Drawing::Point(172, 285);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(275, 27);
+			this->textBox3->TabIndex = 7;
+			this->textBox3->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			this->textBox3->TextChanged += gcnew System::EventHandler(this, &MainForm::textBox3_TextChanged);
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->ForeColor = System::Drawing::Color::Black;
+			this->label5->Location = System::Drawing::Point(61, 355);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(99, 25);
+			this->label5->TabIndex = 8;
+			this->label5->Text = L"Resultado";
+			this->label5->Click += gcnew System::EventHandler(this, &MainForm::label5_Click);
+			// 
+			// textBox4
+			// 
+			this->textBox4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox4->Location = System::Drawing::Point(172, 355);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(275, 27);
+			this->textBox4->TabIndex = 9;
+			this->textBox4->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(550, 511);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->ForeColor = System::Drawing::Color::CornflowerBlue;
+			this->Name = L"MainForm";
+			this->RightToLeft = System::Windows::Forms::RightToLeft::Yes;
+			this->ShowIcon = false;
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label5_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+
+
+	double Tensao = 0.0;
+	double Corrente = 0.0;
+	double Resistencia = 0.0;
+	
+	
+
+	if (textBox1->Text->Length == 0 && textBox2->Text->Length == 0 && textBox3->Text->Length == 0) {
+
+		textBox4->Text = "Digite pelo menos dois valores";
+	}
+	if (textBox1->Text->Length != 0 && textBox2->Text->Length != 0 && textBox3->Text->Length != 0) {
+
+
+		textBox4->Text = "Digite apenas dois valores";
+
+	}
+	if (textBox1->Text->Length == 0 && textBox2->Text->Length != 0 && textBox3->Text->Length != 0) {
+
+		Corrente = Double::Parse(textBox2->Text);
+		Resistencia = Double::Parse(textBox3->Text);
+		Tensao = Corrente * Resistencia;
+		textBox4->Text ="Tensão igual: " + System::Convert::ToString(Tensao) + " Volts";		
+		
+		
+	}
+	if (textBox1->Text->Length != 0 && textBox2->Text->Length != 0 && textBox3->Text->Length == 0) {
+
+		Tensao = Double::Parse(textBox1->Text);
+		Corrente = Double::Parse(textBox2->Text);
+		Resistencia = Tensao / Corrente;
+
+		textBox4->Text = "Resistencia igual: " + System::Convert::ToString(Resistencia) + " Ohms";	
+		
+
+	}
+	if (textBox1->Text->Length != 0 && textBox2->Text->Length == 0 && textBox3->Text->Length != 0) {
+
+		Tensao = Double::Parse(textBox1->Text);
+		Resistencia = Double::Parse(textBox3->Text);
+		Corrente = Tensao / Resistencia;
+		
+		
+
+		textBox4->Text = "Corrente igual: " + System::Convert::ToString(Corrente) + " Amperes";
+
+
+	}
+	if (textBox1->Text->Contains(".") || textBox2->Text->Contains(".") || textBox3->Text->Contains("."))
+	{
+		textBox4->Text = " Use virgula ao inves de ponto";
+	}
+
+	//Tensao = Double::Parse(textBox1->Text);
+	//Corrente = Double::Parse(textBox2-> Text);
+	//Resistencia = Double::Parse(textBox3->Text);
+
+	
+
+
+}
+private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
